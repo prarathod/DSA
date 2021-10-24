24e52fbf02df7e003c505b3953098e2bec1a9f54
prarathod <prajwal_fw11_331@masai.school>
2021-10-24 11:45:45 +0530

    methods

--- Recursion/array_using_recursion.js
+++ Recursion/array_using_recursion.js
@@ -1,6 +1,6 @@
 // 1 print all all elemnet in an array
 
-let arr = [1,2,3,4,5,6,7,8,9,10];
+let arr = [1,2,3,4,5,16,7,8,9,10];
 
 function allElem(arr,index){
 
@@ -21,4 +21,73 @@ function reverse(arr,len){
     return reverse(arr,len-1)
 }
 
-console.log(reverse(arr,arr.length-1))
+// console.log(reverse(arr,arr.length-1))
+
+let str = "Hello mini";
+
+function reverseStr(str,len,ans){
+    if(len<0){
+        console.log(ans);
+        return;
+    }
+
+    ans+= str[len]+" "
+    return reverseStr(str,len-1,ans);
+}
+// reverseStr(str,str.length-1,"")
+
+
+// find given str is paledrom or not;
+
+let str1 = "annna";
+
+function isPalodrom(str,s,e){
+
+    if(s==e){
+        return true;
+    }
+    if(str[s]!=str[e]){
+
+        return false;
+    }
+    if(s<e+1){
+        return isPalodrom(str,s+1,e-1);
+    }
+    return true;
+}
+// console.log(isPalodrom(str1,0,str1.length-1))
+
+
+// search a element index in an array
+
+function search(arr,len,key){
+    if(arr.length==len){
+        return "Key Not found";
+    }
+
+    if(arr[len]==key){
+        return len;
+    }
+
+    return search(arr,len+1,key)
+
+}
+
+// console.log(search(arr,0,16))
+
+
+
+
+
+// largest element in an arrya using recursion
+
+function largestElm(arr , index, elm){
+    if(index==arr.length){
+        return elm;
+    }
+    if(arr[index]>elm){
+        elm= arr[index];
+    }
+    return largestElm(arr,index+1,elm);
+}
+console.log(largestElm(arr,0,-999900000999))
