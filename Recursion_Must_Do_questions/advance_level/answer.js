465b6c3f5b3600f3c953c4352407103ba9167c8b
prarathod <prajwal_fw11_331@masai.school>
2021-11-07 22:50:35 +0530

    recursion array problems

--- Recursion_Must_Do_questions/advance_level/answer.js
+++ Recursion_Must_Do_questions/advance_level/answer.js
@@ -35,3 +35,81 @@ function search1(arr,target,s,e){
 }
 
 // console.log(search1(arr,,0,arr.length-1))
+
+let num = 1236;
+let a = num.toString()
+
+
+function sum(str,index,total){
+    if(index>=str.length){
+     
+        return total;
+    }
+    total+=Number(str[index]);
+    return sum(str,index+1,total)
+}
+// console.log(sum(a,0,0))
+
+let ar = [1,1,2,3,5,3,6,8,3,7,3,7,9,2,3,4];
+const uniq = [...new Set(ar)]
+// console.log(uniq)
+
+// 3rd
+
+function countNum(str,count,n,target){
+    
+    if(n===str.length){
+      
+        return count;
+    }
+    if(str[n]==target){
+        
+        count++;
+    }
+    return countNum(str,count,n+1,target)
+}
+
+// console.log(countNum('12030204454602350',0,0,0))
+
+// 4th
+
+function uptoZero(num,steps){
+ 
+    if(num==0){
+        return steps;
+    }
+    if(num%2==0){
+       return uptoZero(num/2,steps+1);
+    }else{
+      return  uptoZero((num-1)/2,steps+1);
+    }
+
+}
+// console.log(uptoZero(4 ,0))
+
+// 5th
+
+function checkShort(arr,i){
+    if(i==arr.length-1){
+        return true;
+    }
+    if(arr[i]>arr[i+1]){
+        return false;
+    }
+    return checkShort(arr,i+1)
+}
+
+// console.log(checkShort([1,2,3,41,5,6,7,8,9],0))
+
+// 6th
+
+function index(arr , target , i){
+    if(arr[i]==target){
+        return i;
+    }
+    if(i==arr.length){
+        return "Traget not present in array"
+    }
+    return index(arr,target,i+1)
+}
+console.log(index([1,2,3,4,5,6,7,8,9],1,0))
