40c2da00f45aae736cb4283bcfc7e2193d4ffee0
prarathod <prajwal_fw11_331@masai.school>
2021-11-09 13:39:33 +0530

    recursion

---
+++ Recursion_Must_Do_questions/level_1/gogAnswer.js
@@ -0,0 +1,13 @@
+function sumArr (arr,len ){
+    if(len<1){
+        return;
+    }
+
+    let temp = new Array(len-1)
+    for(var i = 0;i<len;i++){
+        console.log(i)
+    }
+}
+let arr = [1,2,3,4,5];
+let len = arr.length;
+sumArr(arr,len)
