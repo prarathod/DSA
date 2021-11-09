4e806fcdfe4f04bf4499d969f09625b719cdea64
prarathod <prajwal_fw11_331@masai.school>
2021-11-09 14:00:14 +0530

    recursion questions

--- Recursion_Must_Do_questions/level_1/gogAnswer.js
+++ Recursion_Must_Do_questions/level_1/gogAnswer.js
@@ -1,13 +1,63 @@
 function sumArr (arr,len ){
-    if(len<1){
-        return;
+    let temp = []
+    if(len<=1){
+        return temp;
     }
 
-    let temp = new Array(len-1)
-    for(var i = 0;i<len;i++){
-        console.log(i)
+
+    for(var i = 0;i<len-1;i++){
+        temp.push(arr[i]+arr[i+1])
     }
+    sumArr(temp,temp.length)
+    console.log(temp)
 }
 let arr = [1,2,3,4,5];
 let len = arr.length;
-sumArr(arr,len)
+// sumArr(arr,len)
+
+
+
+
+// 2nd
+
+function maxMin(arr,max=-9999,min=99999,i=0){
+    if(i==arr.length){
+        console.log('max:-',max,', min:- ',min);
+        return;
+    }
+    if(max<arr[i]){
+        max = arr[i]
+    }
+    if(min>arr[i]){
+        min = arr[i]
+    }
+    return maxMin(arr,max,min,i+1);
+}
+
+// console.log(maxMin([1, 4, 45, 6, 10, -8]))
+
+
+// 3rd:- 
+
+function upperCase(str,i=0){
+    if(str[i]=="A"||str[i]=="B"||str[i]=="C"||str[i]=="D"||str[i]=="E"||str[i]=="F"||str[i]=="G"||str[i]=="H"||str[i]=="I"||str[i]=="J"||str[i]=="K"||str[i]=="L"||str[i]=="M"||str[i]=="N"||str[i]=="O"||str[i]=="P"||str[i]=="Q"||str[i]=="R"||str[i]=="S"||str[i]=="T"||str[i]=="U"||str[i]=="V"||str[i]=="W"||str[i]=="X"||str[i]=="Y"||str[i]=="Z "){
+        return str[i];
+    }
+    if(str.length==i){
+        return -1;
+    }
+    return upperCase(str,++i)
+}
+console.log(upperCase("geeksforgees"))
+// or
+
+function touppder(str,i=0){
+    if(str[i]===str[i].toUpperCase()){
+        return str[i];
+    }
+    if(str.length==i){
+        return -1;
+    }
+    return touppder(str,++i);
+}
+console.log(touppder("prajwwaL"))
