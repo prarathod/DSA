185b776c2aeb8e7ebed16d5ecbe58a18d0ff2fd9
prarathod <prajwal_fw11_331@masai.school>
2021-11-21 21:46:31 +0530

    binary search

--- Binary_search/binarySearchAssending.js
+++ Binary_search/binarySearchAssending.js
@@ -103,4 +103,89 @@ function longest(arr,target){
     return result;
 }
 
-console.log(longest([1,1,1,1,1,1,2,3,4,4,4,6,7,8,9],1))
+// console.log(longest([1,1,1,1,1,1,2,3,4,4,4,6,7,8,9],1))
+
+
+// find the count
+function last(arr,target){
+    let start = 0;
+    let end = arr.length-1;
+    let result=-1;
+    while(start<=end){
+        let mid = start + Math.floor((end-start)/2);
+        if(arr[mid]==target){
+            result = mid;
+            start = mid+1;
+        }
+        if(arr[mid]<target){
+            start = mid+1;
+        }
+        if(arr[mid]>target){
+            end = mid-1;
+        }
+    }
+    return result;
+}
+
+function first(arr,target){
+    let start = 0;
+    let end = arr.length-1;
+    let result = -1;
+    while(start <=end){
+        let mid = start + Math.floor((end-start)/2);
+        if(arr[mid]==target){
+            result = mid;
+            end = mid-1;
+        }
+        if(arr[mid]<target){
+            start = mid+1;
+        }
+        if(arr[mid]>target){
+            end= mid-1;
+        }
+    }
+    return result;
+}
+function count(arr,target){
+    return last(arr,target)-first(arr,target)+1;
+}
+// console.log(count([1,2,3,4,4,4,4,4,4,5,5,5,5,5,6,7,8],4))
+
+
+// find the number of rotation of an array .
+
+function rotation (arr){
+    let count = 0;
+    let start = 0;
+    let end = arr.length-1;
+    console.log("1")
+    
+    if(arr.length==1){
+        console.log("2")
+        return 0;
+        
+    }
+    if(arr[start]<=arr[end]){
+        console.log("3")
+        return 0;
+    }
+    while (start<=end){
+        console.log(arr[start],arr[end])
+        let mid = start + Math.floor((end-start)/2);
+        if((arr[mid]<(arr[(mid+1)%arr.length]))&& arr[mid]<arr[mid+arr.length-1]%arr.length){
+            count = mid;
+            console.log("1")
+        }
+        if(arr[start]<=arr[mid]){
+            start = mid+1;
+            console.log("5")
+        }
+        if(arr[mid]<=arr[end]){
+            end = mid-1;
+            console.log("6")
+        }
+    }
+    return count;
+}
+
+console.log(rotation([11,12,13,14,15,16,16,2,3,4]))
