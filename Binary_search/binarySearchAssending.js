2689c71b39c5e4d43468af5749087b5e869b3985
prarathod <prajwal_fw11_331@masai.school>
2021-11-21 19:30:26 +0530

    binary search

--- Binary_search/binarySearchAssending.js
+++ Binary_search/binarySearchAssending.js
@@ -55,5 +55,28 @@ function search(arr,target){
 
 let ar1 = [9,8,7,6,5,4,3,2,1]
 let ar2 = [1,2,3,4,5,6,7,8,9]
-console.log(search(ar1,1));
-console.log(search(ar2,6));
+// console.log(search(ar1,1));
+// console.log(search(ar2,6));
+
+
+function nearest(arr, target){
+    let start = 0;
+    let end = arr.length-1;
+    let result = -1;
+    while(start<=end){
+        let mid = start + Math.floor((end-start )/2);
+        if(arr[mid]==target){
+            result = mid;
+            end = mid-1;
+        }
+        if(arr[mid]<target){
+            start = mid+1;
+        }
+        if(arr[mid]>target){
+            end = mid-1;
+        }
+
+    }
+    return result;
+}
+// console.log(nearest([1,2,2,2,2,2,2,2],2))
