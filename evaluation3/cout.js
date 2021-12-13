6fa220adfe16de288edfd0c457b3f7b6cf202b42
prarathod <prajwal_fw11_331@masai.school>
2021-12-13 11:47:05 +0530

    count game

--- evaluation3/cout.js
+++ evaluation3/cout.js
@@ -1,16 +1,33 @@
+function count(arr){
+    let char="";
+    let count = 0;
+    if(arr.length==0){
+        return 0;
+    }
+    arr.forEach((elm)=>{
+        if(char != elm){
+            count ++;
+            char = elm;
+        }
+    });
+    return count;
+}
+
 
 function runProgram(input) {
     let ar = input.split("\n");
-    let virat = ar[0].trim().split("");
-    let rohit = ar[1].trim().split("");
-console.log(ar)
+    for(var i = 1 ; i<ar.length;i++){
+        let arr = ar[i].trim().split("");
+        console.log(count(arr))
+    }
  
 
 }
 
 if (process.env.USERNAME === 'prajw') {
-  runProgram(`AEDFHR
-  ABCDGH`);
+  runProgram(`2
+  aaaaa
+  abbbaaz`);
 } else {
   process.stdin.resume();
   process.stdin.setEncoding("ascii");
