b2a577aa5cfe0f5625bd12dbb6a7dd8ae9d377ff
prarathod <prajwal_fw11_331@masai.school>
2021-12-27 12:08:24 +0530

    evalution

--- evaluatin/eval/presentOnce.js
+++ evaluatin/eval/presentOnce.js
@@ -1,19 +1,35 @@
 
 function runProgram(input) {
     let input1 = input.split("\n")
-    let ar = input1[1].trim().split(" ").map(Number);
+    for(var i = 2 ; i<input1.length;i=2+i){
+        let ar = input1[i].trim().split(" ").map(Number);
+
+        let obj = {};
+        ar.forEach((elm)=>{
+            if(obj[elm] == undefined){
+                obj[elm] = 1
+            }else{
+                obj[elm] = 2
+            }
+        })
+        let Obj1 = Object.entries(obj)
+            Obj1.forEach((elm)=>{
+                if(elm[1]==1){
+                    console.log(elm[0])
+                }
+
+            })
+
+    }
+   
 
 
 }
 
 if (process.env.USERNAME === 'prajw') {
-    runProgram(`5
-    10 1 10 3 4
-    4
-    1 10
-    2 9
-    3 4
-    2 2`);
+    runProgram(`1
+    5
+    1 2 1 3 4 4 5 2 3`);
 } else {
     process.stdin.resume();
     process.stdin.setEncoding("ascii");
