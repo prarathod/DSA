5c0e14cff46c6fbfabe65c05cfc273f782e8e363
prarathod <prajwal_fw11_331@masai.school>
2022-01-17 13:42:57 +0530

    evalutions

--- evaluation3/commont.js
+++ evaluation3/commont.js
@@ -12,19 +12,22 @@ var checkWavePattern = function (head) {
         hn = hn.next
         temp = -1;
     }
+    let c1 = 0;
+    let c2 = 0;
+    let c3 = 0;
+    let c4  = 0;
     if (hn.data < hn.next.data) {
         hn = hn.next;
         temp = 1;
     }
     while (hn.next !== null) {
+        // console.log(head)
         if (temp == -1 && hn.data > hn.next.data) { return false} 
         if (temp == 1 && hn.data < hn.next.data) { return false }
         temp = temp * -1;
         hn = hn.next
     }
-    
     return output;
-
 };
 
 
