8bfaec6b82c4dbef5464d8f1c62dd99d218e0405
prarathod <prajwal_fw11_331@masai.school>
2022-03-03 10:34:07 +0530

    arr Part of forur

--- evaluation3/arrPartOfforur.js
+++ evaluation3/arrPartOfforur.js
@@ -1,28 +1,31 @@
 
-function nways(n){
-    if(n==1 || n==0){
-        return 1;
-    };
-    if(n<0){
-        return 0;
-    }
-
-    return nways(n-1)+nways(n-2)+nways(n-3);
-}
-
-
-
 function runProgram(input) {
     let a = input.split("\n");
-    let k = a[0].trim().split(" ").map(Number)[0];
-    console.log(nways(k))
+    let ar = a[1].trim().split(" ").map(Number);
+    let div = ar.length/4;
     
+    let c1 = 0,c2=0,c3=0,c4=0;
+
+    for(var i =0;i<ar.length;i++){
+        if(i<div){
+            c1+=ar[i];
+        }else if(i>=div && i<div*2){
+            c2+=ar[i];
+        }else if(i>=div*2 && i<div*3){
+            c3+=ar[i];
+        }else{
+            c4+=ar[i];
+        }
+    }
+    let equation = 2*c1 + c2 + 2*c3 + c4
+    console.log(equation)
 
 
 }
 
 if (process.env.USERNAME === 'prajw') {
-    runProgram(`4`);
+    runProgram(`8
+    1 2 3 4 5 6 7 8`);
 } else {
     process.stdin.resume();
     process.stdin.setEncoding("ascii");
