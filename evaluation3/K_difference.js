72d549efe54b781436c986990fea95a2c28740d5
prarathod <prajwal_fw11_331@masai.school>
2022-03-13 11:13:18 +0530

    add

--- evaluation3/K_difference.js
+++ evaluation3/K_difference.js
@@ -1,61 +1,18 @@
-
-
-function difference(arr, k) {
-    let start = 0;
-    let end = arr.length - 1;
-
-    while (start < end) {
-        if (Math.abs(arr[end] - arr[start]) == k) {
-            return "Yes";
-        }
-        if (arr[end] - arr[start] < k) {
-            start++;
-        } else {
-            end--
-        }
+let max = 10000;
+let dp = Array.from({ length: max }, (_, i) => 0);
+function initDP() {
+    for (let i = 0; i < max; i++){
+        dp[i] = -1;
     }
-    return 'No';
 }
-
-
-
-
-
-
-function runProgram(input) {
-    let a = input.split("\n");
-   
-    for(var i =2;i<a.length;i= 2+i){
-        let arr = a[i].trim().split(" ").map(Number);
-        let k = a[i-1].trim().split(" ").map(Number)[1];
-        console.log(difference(arr,k))
+function func(x) {
+    if (x == 0){
+        return 0;
     }
-
-
-
+    if (dp[x] == -1){
+        dp[x] = x + func(Math.floor(x / 2));
+    }
+    return dp[x];
 }
-
-if (process.env.USERNAME === 'prajw') {
-    runProgram(`2
-    5 3
-    1 2 3 4 5
-    5 8
-    1 2 3 4 5 `);
-} else {
-    process.stdin.resume();
-    process.stdin.setEncoding("ascii");
-    let read = "";
-    process.stdin.on("data", function (input) {
-        read += input;
-    });
-    process.stdin.on("end", function () {
-        read = read.replace(/\n$/, "");
-        read = read.replace(/\n$/, "");
-        runProgram(read);
-    });
-    process.on("SIGINT", function () {
-        read = read.replace(/\n$/, "");
-        runProgram(read);
-        process.exit(0);
-    });
-}
+initDP();
+console.log(func(8))
---
+++ evaluation3/RelevelTemplete.js
@@ -0,0 +1,43 @@
+
+// Java - legacy (HotSpot 8u331);
+
+/* package whatever; // don't place package name! */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// /* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	}
+}
+
+
+
+//  Java(HotSpot 17.0.1);
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	}
+}
+
+
+
+
+
+
+
+
+
